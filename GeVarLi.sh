88db755dbd58b5d811a3e6d1e4be6ba360700660
Nicolas FERNANDEZ NUÑEZ <fernandez.nunez.nicolas@gmail.com>
2021-12-08 18:55:46 +0100

    Corrections before internal IRD_TransVIHMI release

--- GeVarLi.sh
+++ GeVarLi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
                                                                                                                            
-##### About #####
+###### About ######
 echo ""
 echo "##### ABOUT #####"
 echo "-----------------"
@@ -10,89 +10,104 @@ echo "Author: Nicolas Fernandez"
 echo "Affiliation: IRD_U233_TransVIHMI"
 echo "Aim: SARS-CoV-2 Genome assembling, Variant and Lineage (pangolin) calling"
 echo "Date: 2021.10.12"
-echo "Run: snakemake --use-conda -s gevarli.smk --cores"
-echo "Latest modification: 2021.11.26"
+echo "Run: snakemake -s path/to/gevarli.smk --cores --use-conda"
+echo "Latest modification: 2021.12.08"
 echo "Todo: ..."
 
 echo "________________________________________________________________________"
 
-##### Hardware check #####
+###### Hardware check ######
 echo ""
 echo "##### HARDWARE #####"
 echo "--------------------"
+time (
 
-physicalcpu=$(sysctl -n hw.physicalcpu)   # Get physical cpu
-echo "Physical CPU: ${physicalcpu}"       # Print physical cpu
-logicalcpu=$(sysctl -n hw.logicalcpu)     # Get logical cpu
-echo "Logical CPU: ${logicalcpu}"         # Print logical cpu
-hwmemsize=$(sysctl -n hw.memsize)         # Get memory size
-ramsize=$(expr $hwmemsize / $((1024**3))) # 1024**3 = GB
-echo "System Memory: ${ramsize} GB"       # Print RAM size
+physicalcpu=$(sysctl -n hw.physicalcpu)     # Get physical cpu
+echo "Physical CPU: ${physicalcpu}"         # Print physical cpu
+
+logicalcpu=$(sysctl -n hw.logicalcpu)       # Get logical cpu
+echo "Logical CPU: ${logicalcpu}"           # Print logical cpu
+
+hwmemsize=$(sysctl -n hw.memsize)           # Get memory size
+ramsize=$(expr ${hwmemsize} / $((1024**3))) # 1024**3 = GB
+echo "System Memory: ${ramsize} GB"         # Print RAM size
 
 echo "________________________________________________________________________"
 
-##### Working directory #####
+###### Working directory ######
 echo ""
 echo "##### WORKING DIRECTORY #####"
 echo "-----------------------------"
 
 workdir=${0%/*}
-echo "CWD: ${workdir}"
+echo "CWD: ${workdir}/"
+
+fastq=$(ls -l ${workdir}/resources/reads/*fastq.gz | wc -l)
+echo "Files: ${fastq}"
 
 echo "________________________________________________________________________"
 
-###### Rename samples #####
+###### Rename samples ######
 # de/comment first line if you want to keep or remove barcode-ID in sample name
 echo ""
 echo "##### RENAME FASTQ FILES #####"
 echo "------------------------------"
 
-#rename -v 's/_S\d+_/_/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null                # Remove barcode-ID like {_S001_}, quiet: 2> /dev/null
-rename -v 's/_L\d+_/_/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null                # Remove line-ID ID like {_L001_}, quiet: 2> /dev/null
-rename -v 's/_\d+.fastq.gz/.fastq.gz/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null # Remove end-name ID like {_001}.fastq.gz, quiet: 2> /dev/null
-echo "Quiet."
+rename -v 's/_S\d+_/_/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null                # Remove barcode-ID like {_S001_}
+rename -v 's/_L\d+_/_/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null                # Remove line-ID ID like {_L001_}
+rename -v 's/_001.fastq.gz/.fastq.gz/' ${workdir}/resources/reads/*.fastq.gz 2> /dev/null # Remove end-name ID like {_001}.fastq.gz
 
 echo "________________________________________________________________________"
 
-###### Call snakemake pipeline #####
+###### Call snakemake pipeline ######
 echo ""
 echo "##### SNAKEMAKE PIPELINE #####"
 echo "-----------------------------"
 
-echo ""
 echo "Unlocking working directory:"
+# Specify working directory (relative paths in the snakefile will use this as their origin)
+# The workflow definition in form of a snakefile.
+# Remove a lock on the working directory.
 snakemake \
-    --directory ${workdir} \
+    --directory ${workdir}/ \
     --snakefile ${workdir}/workflow/rules/gevarli.smk \
-    --cores \
-    --use-conda \
-    --printshellcmds \
-    --keep-going \
-    --rerun-incomplete \
-    --unlock # unlock first, if previous error
+    --unlock
 
 echo ""
 echo "Dry run:"
+# The workflow definition in form of a snakefile.
+# Specify working directory (relative paths in the snakefile will use this as their origin)
+# Use at most N CPU cores/jobs in parallel. If N is omitted or ‘all’, the limit is set to the number of available CPU cores.
+# If defined in the rule, run job in a conda environment.
+# Do not execute anything, and display what would be done. If you have a very large workflow, use –dry-run –quiet to just print a summary of the DAG of jobs.
+# Do not output any progress or rule information.
 snakemake \
-    --directory ${workdir} \
+    --directory ${workdir}/ \
+    --snakefile ${workdir}/workflow/rules/gevarli.smk \
+    --cores \
+    --use-conda \
+    --dryrun \
+    --quiet
+
+echo ""
+echo "Let's go!"
+# The workflow definition in form of a snakefile.
+# Specify working directory (relative paths in the snakefile will use this as their origin)
+# Use at most N CPU cores/jobs in parallel. If N is omitted or ‘all’, the limit is set to the number of available CPU cores.
+# If defined in the rule, run job in a conda environment.
+# Print out the shell commands that will be executed.
+# Go on with independent jobs if a job fails.
+# Re-run all jobs the output of which is recognized as incomplete.
+# Tell the scheduler to assign creation of given targets (and all their dependencies) highest priority.
+snakemake \
+    --directory ${workdir}/ \
     --snakefile ${workdir}/workflow/rules/gevarli.smk \
     --cores \
     --use-conda \
     --printshellcmds \
     --keep-going \
     --rerun-incomplete \
-    --dryrun # then dry run, check error like missing file
-
-echo ""
-echo "Let's go!"
-snakemake \
-    --directory ${workdir} \
-    --snakefile ${workdir}/workflow/rules/gevarli.smk \
-    --cores \
-    --use-conda \
-    --printshellcmds \
-    --keep-going \
-    --rerun-incomplete # last, real run 
+    --prioritize multiqc_reports_aggregation
 
 echo "________________________________________________________________________"
 
@@ -103,71 +118,60 @@ echo "--------------------------------------"
 
 mkdir ${workdir}/results/graphs/ 2> /dev/null
 
-for graph in "dag rulegraph filegraph" ; do
-    for extention in "pdf png" ; do
+graphList="dag rulegraph filegraph"
+extentionList="pdf png"
+
+for graph in ${graphList} ; do
+    for extention in ${extentionList} ; do
 	snakemake \
-            --directory ${workdir} \
+	    --directory ${workdir}/ \
             --snakefile ${workdir}/workflow/rules/gevarli.smk \
-            --$graph | dot -T$extention > ${workdir}/results/graphs/$graph.$extention ;
+            --${graph} | \
+	    dot -T${extention} > \
+		${workdir}/results/graphs/${graph}.${extention} ;
     done ;
 done
 
 echo "________________________________________________________________________"
 
-
-###### Rename all fasta header with sample name ######
-echo ""
-echo "##### RENAME FASTA HEADERS ######"
-echo "---------------------------------"
-
-for fasta in ${workdir}/results/bcftools/*_consensus.fasta ; do
-    sample=$(basename $fasta \_consensus\.fasta) ;
-    echo $sample ;
-    sed "s/^>.*$/>$sample/" $fasta > ${workdir}/results/$sample.tmp &&
-	mv ${workdir}/results/$sample.tmp $fasta ;
-done
-echo "________________________________________________________________________"
-
 ###### Concatenate all consensus fasta in one unique fasta file ######
 echo ""
-echo "##### CONCATENATE FASTA FILES ######"
-echo "------------------------------------"
+echo "##### CONCATENATE FASTA FILES #####"
+echo "-----------------------------------"
 
 cat ${workdir}/results/bcftools/*_consensus.fasta > ${workdir}/results/All_cconsensus_sequences.fasta
 
 echo "________________________________________________________________________"
 
-###### Rename all pangolin report header with sample name ######
-echo ""
-echo "##### RENAME LINEAGE taxon ID ######"
-echo "-------------------------------------"
-
-for report in ${workdir}/results/pangolin/*_lineage_report.csv ; do
-    sample=$(basename $report \_lineage\_report\.csv) ;
-    echo $sample ;
-    taxon=$(awk '{if(NR==2){ print $1; }}' $report) ;
-    sed "s/$taxon/$sample,/" $report > ${workdir}/results/$sample.tmp &&
-	mv ${workdir}/results/$sample.tmp $report ;
-done
-
-echo "________________________________________________________________________"
-
 ###### Concatenate all Pangolin lienage reports ######
 echo ""
-echo "##### CONCATENATE PANGOLIN REPORTS ######"
-echo "-----------------------------------------"
+echo "##### CONCATENATE PANGOLIN REPORTS #####"
+echo "----------------------------------------"
 
 cat ${workdir}/results/pangolin/*_lineage_report.csv > ${workdir}/results/All_Pangolin_lineage_reports.csv
-awk "NR==1 || NR%2==0" ${workdir}/results/All_Pangolin_lineage_reports.csv > ${workdir}/results/Pangolin.tmp &&
-    mv ${workdir}/results/Pangolin.tmp ${workdir}/results/All_Pangolin_lineage_reports.csv
+
+awk "NR==1 || NR%2==0" ${workdir}/results/All_Pangolin_lineage_reports.csv > ${workdir}/results/Pangolin.tmp \
+    && mv ${workdir}/results/Pangolin.tmp ${workdir}/results/All_Pangolin_lineage_reports.csv
+
 sed "s/,/\t/g" ${workdir}/results/All_Pangolin_lineage_reports.csv > ${workdir}/results/All_pangolin_lineage_reports.tsv
 
 echo "________________________________________________________________________"
 
-
-###### END ######
+###### Report time ######
 echo ""
-echo "##### SCRIPT END ######"
-echo "-----------------------"
+echo "##### TIMER #####"
+echo "-----------------"
+echo "Processing time:"
+)
+
+echo "________________________________________________________________________"
+
+###### End ######
+echo ""
+echo "##### SCRIPT END #####"
+echo "----------------------"
+
+find ${workdir}/results -type f -empty -print -delete # Remove empty file (like empty log)
+find ${workdir}/results -type d -empty -print -delete # Remove empty directory
 
 echo "________________________________________________________________________"
